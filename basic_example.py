5afbf760c10877e9e8518a54ae6eaf365ad1ff73
Pedro Coelho <pedro-luiz.coelho-rodrigues@gipsa-lab.fr>
2019-03-18 11:10:36 +0100

    basic_example.py its done

--- basic_example.py
+++ basic_example.py
@@ -1,65 +1,95 @@
 
-import numpy as np
-import mne
 from virtualreality.dataset import VirtualReality
 from virtualreality.utilities import get_block_repetition
 
+import numpy as np
+import mne
 import moabb
-from moabb.paradigms import P300
+import pandas as pd
 
+from moabb.paradigms import P300
 from pyriemann.estimation import ERPCovariances
 from pyriemann.classification import MDM
+
+from sklearn.preprocessing import LabelEncoder
+from sklearn.metrics import roc_auc_score
+from sklearn.model_selection import KFold
+
+from tqdm import tqdm
+
+"""
+=============================
+Classification of the trials
+=============================
+
+This example shows how to extract the epochs from the dataset of a given
+subject and then classify them using Riemannian Geometry framework for BCI. 
+We compare the scores in the VR and PC conditions.
+
+"""
+# Authors: Pedro Rodrigues <pedro.rodrigues01@gmail.com>
+#
+# License: BSD (3-clause)
 
 import warnings
 warnings.filterwarnings("ignore")
 
-# define the dataset instance
-dataset = VirtualReality(VR=False, PC=True)
+# create dataset
+dataset = VirtualReality()
 
 # get the paradigm
 paradigm = P300()
 
-# get the epochs and labels
-X, labels, meta = paradigm.get_data(dataset, subjects=[1])
+# loop to get scores for each subject
+nsubjects = 5
 
-# # example of how to select the blocks and repetitions of interest
-# block_list = [1, 2, 3]
-# repetition_list = [1]
-# X_select, labels_select, meta_select = get_block_repetition(X, labels, meta, block_list, repetition_list)
-#
-# # split in training and testing datasets
-# block_training = list(range(1, 10+1))
-# repetition_train = [1, 2, 3]
-# X_training, labels_training, meta_training = get_block_repetition(X, labels, meta, block_training, repetition_train)
-#
-# block_test = [11, 12]
-# repetition_test = [1]
-# X_test, labels_test, meta_test = get_block_repetition(X, labels, meta, block_test, repetition_test)
-# repetition_test = [1, 2]
-# X_test2, labels_test2, meta_test2 = get_block_repetition(X, labels, meta, block_test, repetition_test)
-# repetition_test = [1, 2, 3]
-# X_test3, labels_test3, meta_test3 = get_block_repetition(X, labels, meta, block_test, repetition_test)
-#
-# # estimate the extended ERP covariance matrices
-# erpc = ERPCovariances(classes=['Target'])
-# erpc.fit(X_training, labels_training)
-# covs_training = erpc.transform(X_training)
-# covs_test = erpc.transform(X_test)
-# covs_test2 = erpc.transform(X_test2)
-# covs_test3 = erpc.transform(X_test3)
-#
-# # get the classification accuracy (TODO : change to AUC for P300)
-# clf = MDM()
-# clf.fit(covs_training, labels_training)
-# scr = clf.score(covs_test, labels_test)
-# scr2 = clf.score(covs_test2, labels_test2)
-# scr3 = clf.score(covs_test3, labels_test3)
-# print(scr, scr2, scr3)
+scores = []
+for subject in tqdm(dataset.subject_list[:nsubjects]):
+	scores_subject = [subject]
+	for condition in ['VR', 'PC']:
 
-# plotting the P300 signals
-import matplotlib.pyplot as plt
-chi = 6
-fig, ax = plt.subplots(facecolor='white', figsize=(10,6))
-ax.plot(np.linspace(0, 1, 513), np.mean(X[labels == 'Target'], axis=0)[chi,:], color='b')
-ax.plot(np.linspace(0, 1, 513), np.mean(X[labels == 'NonTarget'], axis=0)[chi,:], color='r')
-fig.show()
+		# define the dataset instance
+		if condition is 'VR':
+			dataset.VR = True
+			dataset.PC = False
+		elif condition is 'PC':
+			dataset.VR = False
+			dataset.PC = True
+
+		# get the epochs and labels
+		X, labels, meta = paradigm.get_data(dataset, subjects=[subject])
+		labels = LabelEncoder().fit_transform(labels)
+
+		kf = KFold(n_splits = 6)
+		repetitions = [1, 2]				
+		auc = []
+
+		blocks = np.arange(1, 12+1)
+		for train_idx, test_idx in kf.split(np.arange(12)):
+
+			# split in training and testing blocks
+			X_training, labels_training, _ = get_block_repetition(X, labels, meta, blocks[train_idx], repetitions)
+			X_test, labels_test, _ = get_block_repetition(X, labels, meta, blocks[test_idx], repetitions)
+
+			# estimate the extended ERP covariance matrices with Xdawn
+			dict_labels = {'Target':1, 'NonTarget':0}
+			erpc = ERPCovariances(classes=[dict_labels['Target']])
+			erpc.fit(X_training, labels_training)
+			covs_training = erpc.transform(X_training)
+			covs_test = erpc.transform(X_test)
+
+			# get the AUC for the classification
+			clf = MDM()
+			clf.fit(covs_training, labels_training)
+			labels_pred = clf.predict(covs_test)
+			auc.append(roc_auc_score(labels_test, labels_pred))
+
+		# stock scores
+		scores_subject.append(np.mean(auc))
+
+	scores.append(scores_subject)
+
+# print results
+df = pd.DataFrame(scores, columns=['subject', 'VR', 'PC'])
+print(df)
+
